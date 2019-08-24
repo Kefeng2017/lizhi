e22434f9a693e39936e7dcdffcbb29fdaeed79f6
kkboom <1396956549@qq.com>
2019-08-24 21:25:33 +0800

    人民需要自由

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '//at.alicdn.com/t/font_1345347_qudi28g6mhs.css';
+
+* {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    outline: none;
+    text-decoration: none;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+#app {
+    padding-bottom: 120px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #cecbcb;
+    background-color: rgb(35, 36, 42);
+
+    .line {
+        margin: 1em 0;
+        border-bottom: none;
+        border-color: #91959b;
     }
-  }
 }
 </style>
